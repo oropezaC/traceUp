8c3a16d6af7e2f20a0700bd6e426f245d80b0277
Yahir Orpeza <yahir.oropeza@gmail.com>
2018-08-18 23:25:40 -0500

    traceup

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,12 +1,7 @@
 var request = require('request');
 var clean = require('../lib/cleanMetadata').initClean;
-/*
-var key = 'AIzaSyB4zzVZY92MCqK6DcD8cupCFgpTamSufD0'
-var cx = '014008713717157577643'
-var api = 'https://www.googleapis.com/customsearch/v1/siterestrict?key=';*/
 
 function searchItems(req,res) {
-
   request.get(config.uri+'?key='+config.key+'&cx='+config.cx+config.cxid+'&q='+req.body.item, function (error, response, body) {
     var items = [];
     if (!error) {
